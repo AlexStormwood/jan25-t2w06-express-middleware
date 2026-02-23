8a61c477d27489cb5cb9f89f423b324d849ef070
Alex <alex.stormwood@bigfootds.com>
2026-02-23 19:30:34 +1100

    feat: server has example routes with example middleware

--- src/server.js
+++ src/server.js
@@ -12,6 +12,67 @@ app.get("/", (request, response) => {
 	});
 });
 
+// GET http://localhost:3000/bananas
+app.get("/bananas", (request, response) => {
+	let result = exampleFunction();
+	response.json({
+		message:"Hello, world!",
+		result: result
+	});
+});
+
+// GET http://localhost:3000/oranges
+app.get("/oranges", (request, response) => {
+	let result = exampleFunction();
+	response.json({
+		message:"Hello, world!",
+		result: result
+	});
+});
+
+function exampleFunction(){
+	return 1 + 1;
+}
+
+// app.verb(path, function);
+
+/* app.verb(
+	path,
+	function,
+		next()
+	function,
+		next()
+	function,
+		next()
+	function,
+		response.json(some early exit of the route handler)
+	function
+		response.json but we never reach here, because the previous function responds instead
+	)
+
+
+*/
+
+app.get(
+	"/middlewareExample", 
+
+	// This is a middleware function! 
+	// It is middleware because it can call next()
+	(request, response, next) => {
+		console.log("middleware activated!");
+		next();
+	},
+
+	// This is the final callback in the chain, 
+	// because it does NOT have the ability to call next().
+	(request, response) => {
+		response.json({
+			message:"Middleware route has completed!"
+		})
+	}
+);
+
+
 module.exports = {
 	app
 }
